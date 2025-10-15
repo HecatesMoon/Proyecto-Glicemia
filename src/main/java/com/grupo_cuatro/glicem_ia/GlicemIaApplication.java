7c89cae33b6c319ae384bb319bcef6942b0568f1
JoacoOrbit <88943020+JoacoOrbit@users.noreply.github.com>
2025-10-15 19:05:21 -0300

    inicializa proyecto

---
+++ src/main/java/com/grupo_cuatro/glicem_ia/GlicemIaApplication.java
@@ -0,0 +1,13 @@
+package com.grupo_cuatro.glicem_ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlicemIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlicemIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo_cuatro/glicem_ia/GlicemIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo_cuatro.glicem_ia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlicemIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
