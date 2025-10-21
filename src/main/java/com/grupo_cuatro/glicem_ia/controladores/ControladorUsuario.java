5798c8da394a3f05a2d8705e2717bdac993e77aa
ESCANORS99 <joaomoris0901@gmail.com>
2025-10-21 13:56:24 -0300

    avance2

---
+++ src/main/java/com/grupo_cuatro/glicem_ia/controladores/ControladorUsuario.java
@@ -0,0 +1,5 @@
+package com.grupo_cuatro.glicem_ia.controladores;
+
+public class ControladorUsuario {
+    
+}
---
+++ src/main/java/com/grupo_cuatro/glicem_ia/repositorios/RepositorioUsuario.java
@@ -0,0 +1,5 @@
+package com.grupo_cuatro.glicem_ia.repositorios;
+
+public class RepositorioUsuario {
+    
+}
