3fc32a7791f925c43d0d5501c17c81b53c28e186
ESCANORS99 <joaomoris0901@gmail.com>
2025-10-28 04:26:50 -0300

    Controlador, repositorio y servicio AlimentacionDia por favor hechar un vitazo

--- src/main/java/com/grupo_cuatro/glicem_ia/servicios/ServicioAlimentacionDia.java
+++ src/main/java/com/grupo_cuatro/glicem_ia/servicios/ServicioAlimentacionDia.java
@@ -1,5 +1,42 @@
 package com.grupo_cuatro.glicem_ia.servicios;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.grupo_cuatro.glicem_ia.modelos.AlimentacionDia;
+import com.grupo_cuatro.glicem_ia.repositorios.RepositorioAlimentacionDia;
+
+@Service
 public class ServicioAlimentacionDia {
     
+    private final RepositorioAlimentacionDia repositorioAlimentacionDia;
+
+    public ServicioAlimentacionDia(RepositorioAlimentacionDia repositorioAlimentacionDia){
+        this.repositorioAlimentacionDia = repositorioAlimentacionDia;
+    }
+
+    public AlimentacionDia guardarAlimentacionDia(AlimentacionDia alimentacionDia) {
+        return repositorioAlimentacionDia.save(alimentacionDia);
+    }
+
+    public Optional<AlimentacionDia> buscarPorId(Long id) {
+        return repositorioAlimentacionDia.findById(id);
+    }
+
+    public List<AlimentacionDia> listarPorUsuarioId(Long usuarioId) {
+        return repositorioAlimentacionDia.findByUsuarioId(usuarioId);
+    }
+
+    public List<AlimentacionDia> listarPorUsuarioYFecha(Long usuarioId, LocalDate fecha) {
+        return repositorioAlimentacionDia.findByUsuarioIdAndFecha(usuarioId, fecha);
+    }
+
+    public void eliminarAlimentacionDia(Long id){
+        repositorioAlimentacionDia.deleteById(id);
+    }
+
+
 }
