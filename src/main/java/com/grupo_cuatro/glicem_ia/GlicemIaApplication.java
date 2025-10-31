e7015b20a7f9781c70492ee031c40f7df6b9399f
MaxRivasGit <rivasmaxi123@gmail.com>
2025-10-31 18:49:03 -0300

    config personal aplication ignora controlador Usuario para pruebas

--- src/main/java/com/grupo_cuatro/glicem_ia/GlicemIaApplication.java
+++ src/main/java/com/grupo_cuatro/glicem_ia/GlicemIaApplication.java
@@ -2,9 +2,21 @@ package com.grupo_cuatro.glicem_ia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication@ComponentScan(
+    basePackages = "com.grupo_cuatro.glicem_ia",
+    excludeFilters = @ComponentScan.Filter(
+        type = org.springframework.context.annotation.FilterType.ASSIGNABLE_TYPE,
+        value = com.grupo_cuatro.glicem_ia.controladores.ControladorUsuario.class
+    )
+)
+@EntityScan("com.grupo_cuatro.glicem_ia.modelos")
+@EnableJpaRepositories("com.grupo_cuatro.glicem_ia.repositorios")
 public class GlicemIaApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(GlicemIaApplication.class, args);
